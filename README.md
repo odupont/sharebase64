6653d0a041404d3aac783fb32e36dc7955e580ac
Olivier Dupont <bitbucket@champimouss.net>
2015-02-24 10:34:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Share Base 64 image Android Plugin for Cordova 3.X #
+By odvpont
---
+++ www/sharebase64.js
@@ -0,0 +1,73 @@
+/**
+ * cordova Share Base 64
+ * Copyright (c) Olivier Dupont
+ *
+ */
+ (function(cordova){
+    var ShareB64 = function() {
+
+    };
+
+    ShareB64.prototype.ACTION_SEND = "android.intent.action.SEND";
+    ShareB64.prototype.ACTION_VIEW= "android.intent.action.VIEW";
+    ShareB64.prototype.EXTRA_TEXT = "android.intent.extra.TEXT";
+    ShareB64.prototype.EXTRA_SUBJECT = "android.intent.extra.SUBJECT";
+    ShareB64.prototype.EXTRA_STREAM = "android.intent.extra.STREAM";
+    ShareB64.prototype.EXTRA_EMAIL = "android.intent.extra.EMAIL";
+    ShareB64.prototype.ACTION_CALL = "android.intent.action.CALL";
+    ShareB64.prototype.ACTION_SENDTO = "android.intent.action.SENDTO";
+
+    ShareB64.prototype.startActivity = function(params, success, fail) {
+        return cordova.exec(function(args) {
+            success(args);
+        }, function(args) {
+            fail(args);
+        }, 'ShareB64', 'startActivity', [params]);
+    };
+
+    ShareB64.prototype.hasExtra = function(params, success, fail) {
+        return cordova.exec(function(args) {
+            success(args);
+        }, function(args) {
+            fail(args);
+        }, 'ShareB64', 'hasExtra', [params]);
+    };
+
+    ShareB64.prototype.getUri = function(success, fail) {
+        return cordova.exec(function(args) {
+            success(args);
+        }, function(args) {
+            fail(args);
+        }, 'ShareB64', 'getUri', []);
+    };
+
+    ShareB64.prototype.getExtra = function(params, success, fail) {
+        return cordova.exec(function(args) {
+            success(args);
+        }, function(args) {
+            fail(args);
+        }, 'ShareB64', 'getExtra', [params]);
+    };
+
+
+    ShareB64.prototype.onNewIntent = function(callback) {
+        return cordova.exec(function(args) {
+            callback(args);
+        }, function(args) {
+        }, 'ShareB64', 'onNewIntent', []);
+    };
+
+    ShareB64.prototype.sendBroadcast = function(params, success, fail) {
+        return cordova.exec(function(args) {
+            success(args);
+        }, function(args) {
+            fail(args);
+        }, 'ShareB64', 'sendBroadcast', [params]);
+    };
+
+    window.shareb64 = new ShareB64();
+    
+    // backwards compatibility
+    window.plugins = window.plugins || {};
+    window.plugins.shareb64 = window.shareb64;
+})(window.PhoneGap || window.Cordova || window.cordova);
